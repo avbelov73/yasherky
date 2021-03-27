6d87a50de56e27bfc2df3d38fbd30e3c6392caa1
Andrey Belov <avbelov73@gmail.com>
2021-03-27 19:03:58 +0400

    all

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,146 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" :style="{backgroundImage: 'url(' + bg + ')'}">
+    <div class="container">
+      <template
+          v-for="section in geo"
+      >
+        <Area
+            v-for="area in section.places"
+            :key="area.id"
+            :area="area"
+            :area-class="areaClass(area.id)"
+            :data="prices.find(item => item.id === area.id)"
+            @active="setActiveArea"
+        />
+      </template>
+      <div class="container_bg" :style="{backgroundImage: 'url(' + border + ')'}"></div>
+    </div>
+    <div class="section" v-if="activeSection">
+      <h4>Сектор {{section.id}}</h4>
+      <div>Стоимость сотки {{section.price}} рублей</div>
+      <div>{{section.places}}</div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import c from '@/assets/2.png';
+import border from '@/assets/border.png';
+// import bg from '@/assets/bg.jpg';
+import bg from '@/assets/1.png';
+import Area from '@/components/Area.vue';
+import geo from '@/geo.json';
+import prices from '@/prices.json';
 export default {
   name: 'App',
+  data() {
+    return {
+      bg: bg,
+      geo: geo,
+      c: c,
+      border: border,
+      prices,
+      activeSection: null
+    }
+  },
   components: {
-    HelloWorld
-  }
+    Area
+  },
+  computed: {
+    section() {
+      const section = this.geo.find(i => i.id === this.activeSection);
+      let res = {
+        id: section.id,
+        price: section.price,
+        places: ''
+      }
+      let places = section.places.map(i => i.id);
+      let start = places[0];
+      let counter = start;
+      places.forEach(i => {
+        if(i - counter > 1) {
+          if (res.places.length > 0) {
+            res.places += ', ';
+          }
+          res.places += start + ' - ' + counter;
+          start = counter;
+        }
+        counter = i;
+      })
+      if(res.places.length === 0)
+        res.places = start + ' - ' + counter;
+      return res;
+    }
+  },
+  methods: {
+    click(event) {
+      let container = document.querySelector('.container');
+      console.log(event.pageX - container.offsetLeft, event.pageY - container.offsetTop);
+    },
+    areaClass(id) {
+      const status = this.prices.find(price => price.id === id).sold;
+      if(status === "Забронирован")
+        return 'warning'
+
+      return (status) ? 'danger' : 'success';
+    },
+    setActiveArea(id) {
+      if(id !== null) {
+        this.activeSection = this.geo.find(section => {
+          let t = section.places.find(s => s.id === id)
+          return (t) ? section.id : null
+        }).id;
+      } else {
+        this.activeSection = null
+      }
+    }
+  },
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+body {
+  margin: 0 !important;
+
+}
 #app {
+  width: 867px;
+  height: 800px;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  position: relative;
+  background-size: contain;
+}
+.container {
+  width: 498px;
+  height: 633px;
+  position: absolute;
+  top: 90px;
+  left: 184px;
+  &_bg {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+  }
+}
+.section {
+  position: absolute;
+  bottom: 50px;
+  left: 50px;
+  background: #00d800;
+  color: #fff;
+  padding: 8px 12px;
+  font-size: 12px;
+  border: 1px solid #fff;
+  h4 {
+    margin: 0 0 8px;
+  }
+  div {
+    padding: 8px;
+    border-top: 1px solid #fff;
+  }
 }
 </style>
---
+++ src/components/Area.vue
@@ -0,0 +1,181 @@
+<template>
+  <div>
+  <svg
+      :viewBox="'0 0 ' + width + ' ' + height"
+      :width="width"
+      :height="height"
+      :style="{top: area.co.top + 'px', left: area.co.left + 'px'}"
+  >
+    <polygon
+        :points="polygon"
+        :class="areaClass"
+    ></polygon>
+    <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle">{{area.id}}</text>
+  </svg>
+    <div
+        class="pg"
+        :style="{top: area.co.top + 'px', left: area.co.left + 'px', width: width + 'px', height: height + 'px', 'shape-outside': 'polygon(' + shape + ')', 'clip-path' : 'polygon(' + shape + ')'}"
+        @mouseover="setActiveArea(data.id)"
+        @mouseout="setActiveArea(null)"
+    >
+    </div>
+    <div
+        class="tooltip"
+        :class="areaClass"
+        :style="{top: area.co.top - 84 + 'px', left: area.co.left  + 'px'}"
+    >
+      <div
+          class="tooltip-wrapper"
+          :id="'tooltip-' + data.id"
+          :class="areaClass"
+      >
+        <h4>{{status}}</h4>
+        <div>Участок № {{data.id}}</div>
+        <div>Площадь участка: {{data.area}} кв.м.</div>
+        <div>Цена
+          <template v-if="data.price && data.sold !== true">{{data.price}}</template>
+          <template v-else>-</template>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    area: {
+      type: Object
+    },
+    areaClass: {
+      type: String
+    },
+    data: {
+      type: Object
+    }
+  },
+  computed: {
+    polygon() {
+      const areas = this.area.polygon;
+      let res = [];
+      for (const r in areas) {
+        res.push(areas[r][0] + ',' + areas[r][1])
+      }
+      return res.join(' ');
+    },
+    shape() {
+      const areas = this.area.polygon;
+      let res = [];
+      for (const r in areas) {
+        res.push(areas[r][0] + 'px ' + areas[r][1] + 'px')
+      }
+      return res.join(', ');
+    },
+    width() {
+      const polygon = this.area.polygon;
+      let max = 0;
+      polygon.forEach(item => {
+        if(item[0] > max)
+          max = item[0]
+      })
+      return max
+    },
+    height() {
+      const polygon = this.area.polygon;
+      let max = 0;
+      polygon.forEach(item => {
+        if(item[1] > max)
+          max = item[1]
+      })
+      return max
+    },
+    status() {
+      if(this.data.sold === 'Забронирован')
+        return this.data.sold;
+      else
+        return (this.data.sold) ? 'Продан' : 'Свободен'
+    }
+  },
+  methods: {
+    setActiveArea(id) {
+      this.$emit('active', id)
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+svg {
+  position: absolute;
+  text {
+    fill: #000;
+    font-size: 8px;
+    font-weight: bold;
+  }
+}
+.success {
+  fill: #00d800;
+  background: #00d800;
+  &:after {
+    border-top-color: #00d800 !important;
+  }
+}
+.warning {
+  fill: #ffc600;
+  background: #ffc600;
+  &:after {
+    border-top-color: #ffc600 !important;
+  }
+}
+.danger {
+  fill: #ff5100;
+  background: #ff5100;
+  &:after {
+    border-top-color: #ff5100 !important;
+  }
+}
+.pg {
+  position: absolute;
+  z-index: 999;
+  &:hover + .tooltip {
+    display: block;
+  }
+}
+.tooltip {
+  position: absolute;
+  display: none;
+  z-index:10;
+  min-width: 195px;
+  &-wrapper {
+    color: #fff;
+    padding: 8px 12px;
+    border: 2px solid #fff;
+    font-size: 12px;
+    position: relative;
+    h4 {
+      margin: 0 0 8px;
+    }
+    &:before {
+      content: '';
+      width: 0;
+      height: 0;
+      border-left: 10px solid transparent;
+      border-right: 10px solid transparent;
+      border-top: 10px solid #fff;
+      position: absolute;
+      bottom: -10px;
+      left: 0;
+    }
+    &:after {
+      content: '';
+      width: 0;
+      height: 0;
+      border-left: 7px solid transparent;
+      border-right: 7px solid transparent;
+      border-top: 7px solid transparent;
+      position: absolute;
+      bottom: -7px;
+      left: 3px;
+    }
+  }
+
+}
+</style>
